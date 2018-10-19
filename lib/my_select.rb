f515d6afa4df08ae638584d23959009c22452872
Paul Clark <clark.paul123@gmail.com>
2018-10-19 18:38:33 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  i2 = 0
+  helper = []
+  selected = []
+    while i < collection.length
+      helper.push(yield collection[i]) 
+        i += 1
+    end
+  
+    while i2 < helper.length
+      if helper[i2] == true 
+      selected.push(collection[i2]) 
+        i2 += 1
+      else 
+        i2 += 1
+      end
+    end
+  selected
 end
