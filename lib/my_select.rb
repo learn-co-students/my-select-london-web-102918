2a7a2324269056b57af09f677826c4f4586af7c4
itzcookiie <mayowaadeniyi@hotmail.com>
2018-10-29 04:04:43 +0000

    Done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
  # your code here!
+    i = 0
+    newarray = []
+    while i < collection.length
+        if yield(collection[i])
+            newarray << collection[i]
+        end
+        i += 1
+    end
+    newarray
 end
+
